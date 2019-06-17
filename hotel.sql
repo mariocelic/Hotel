48a163b7ec1f693432e36c33dd16f9c3ad41d5ad
mariocelic <mariocelic@gmail.com>
2019-06-17 16:36:56 +0200

    Ispravljene greske

--- hotel.sql
+++ hotel.sql
@@ -7,29 +7,23 @@ create database hotel default character set utf8;
 use hotel;
 
 
-create table zaposlenik(
-sifra int not null primary key auto_increment,
-ime varchar(50) not null,
-prezime varchar(50) not null,
-zanimanje varchar(50) not null,
-IBAN char(32)
-);
 
 create table soba(
 sifra int not null primary key auto_increment,
 vrstasobe int not null,
 slikasobe int not null,
-opissobe text
+opissobe text,
+hotel int not null
 );
+
 
 create table hotel(
 sifra int  not null primary key auto_increment,
 ime varchar(50) not null,
 grad varchar(50) not null,
-drzava varchar(50) not null,
-zaposlenik int not null,
-soba int not null 
+drzava varchar(50) not null
 );
+
 
 create table vrstasobe(
 sifra int not null primary key auto_increment,
@@ -39,7 +33,7 @@ cijenasobe decimal(18,2)
 
 create table slikasobe(
 sifra int not null primary key auto_increment,
-slika blob
+slika varchar(50) not null
 );
 
 
@@ -48,13 +42,8 @@ sifra int not null primary key auto_increment,
 kupac int not null,
 placanje int not null,
 datumprijave datetime not null,
-datumodjave datetime not null
-);
-
-create table dogadjaj(
-sifra int not null primary key auto_increment,
-soba int not null,
-rezervacija int not null
+datumodjave datetime not null,
+soba int not null
 );
 
 
@@ -75,17 +64,17 @@ rezervacija int not null,
 nacinplacanja boolean not null
 );
 
-alter table hotel add foreign key (zaposlenik) references zaposlenik(sifra);
-alter table hotel add foreign key (soba) references soba(sifra);
+
+alter table soba add foreign key (hotel) references hotel(sifra);
 
 alter table soba add foreign key (vrstasobe) references vrstasobe(sifra);
 alter table soba add foreign key (slikasobe) references slikasobe(sifra);
 
 alter table rezervacija add foreign key (kupac) references kupac(sifra);
 alter table rezervacija add foreign key (placanje) references placanje(sifra);
+alter table rezervacija add foreign key (soba) references soba(sifra);
 
-alter table dogadjaj add foreign key (soba) references soba(sifra);
-alter table dogadjaj add foreign key (rezervacija) references rezervacija(sifra);
+
 
 
 
